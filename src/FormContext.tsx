1154eeb4d2f8860b9469e09795b4f3dc06bb4dc8
Joost Lubach <joostlubach@gmail.com>
2023-07-11 11:06:09 +0200

    Working on form

--- src/FormContext.tsx
+++ src/FormContext.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useTimer } from 'react-timer'
+import { forwardRef } from 'react-util'
+import { assignRef, releaseRef, useContinuousRef } from 'react-util/hooks'
+import { isFunction, isObject, some } from 'lodash'
+import { translateFormModelErrorPaths } from './errors'
+import { FormTranslationFunctions, FormTranslationProvider } from './FormTranslationContext'
+import { useFormDataSource } from './hooks'
 import {
   FieldChangeCallback,
   FormData,
   FormDataKey,
   FormError,
   FormModel,
+  isSuccessResult,
   SubmitFunction,
+  SubmitOptions,
+  SubmitResult,
 } from './types'
 
 export interface FormContext<M extends FormModel> {
@@ -63,24 +73,218 @@ export const FormContext = React.createContext<FormContext<any>>({
   reset:      () => void 0,
 })
 
-const emptyFieldChangeCallback = (() => void 0) as any as FieldChangeCallback<any>
-emptyFieldChangeCallback.partial = () => void 0
-
 //------
-// FormContainer
+// FormProvider]
 
-export function FormContainer(props: FormContainerProps) {
-  return (
-    <FormContext.Provider value={props.form}>
-      {props.children}
-    </FormContext.Provider>
+export interface FormProviderProps<M extends FormModel> {
+  model:           M
+  initialData?:    FormData<M>
+  autoSubmit?:     boolean
+  resetOnSuccess?: boolean
+
+  translation?: FormTranslationFunctions
+
+  beforeSubmit?: (model: M) => boolean | undefined
+  afterSubmit?:  (result: SubmitResult, model: M) => any
+
+  children?: React.ReactNode | ((form: FormContext<M>) => React.ReactNode)
+}
+
+export const FormProvider = forwardRef('FormProvider', <M extends FormModel>(props: FormProviderProps<M>, ref: React.Ref<FormContext<M>>) => {
+
+  const {
+    model,
+    initialData,
+    resetOnSuccess = false,
+    autoSubmit,
+    beforeSubmit,
+    afterSubmit,
+    translation,
+    children
+  } = props
+
+  const [modified, setModifiedState] = React.useState<boolean>(false)
+  const [errors, setErrorsState]     = React.useState<FormError[]>([])
+  const [submitting, setSubmitting]  = React.useState<boolean>(false)
+
+  const modifiedRef    = React.useRef<boolean>(false)
+  const initialDataRef = useContinuousRef(initialData)
+
+  const setModified = React.useCallback((value: boolean) => {
+    if (value === modifiedRef.current) { return }
+    setModifiedState(modifiedRef.current = value)
+  }, [])
+
+  //------
+  // Invalidation
+
+  const invalid = errors.length > 0
+
+  const isInvalid = React.useCallback(
+    (field: FormDataKey<M>) => some(errors, error => error.field === field),
+    [errors],
   )
-}
 
-export interface FormContainerProps {
-  form:      FormContext<any>
-  children?: React.ReactNode
-}
+  const errorsFor = React.useCallback((field: FormDataKey<M>, includeChildren: boolean = false) => {
+    return errors.filter(error => {
+      if (error.field === field) { return true }
+      if (includeChildren && error.field?.startsWith(`${String(field)}.`)) { return true }
+      return false
+    })
+  }, [errors])
+
+  const errorsRef = React.useRef<FormError[]>(errors)
+  const addError = React.useCallback((error: FormError) => {
+    const newErrors = [
+      ...errorsRef.current,
+      error,
+    ]
+    setErrorsState(errorsRef.current = newErrors)
+  }, [])
+
+  const clearErrors = React.useCallback((field?: FormDataKey<M>) => {
+    if (field == null) {
+      setErrorsState(errorsRef.current = [])
+    } else {
+      setErrorsState(errorsRef.current = errorsRef.current.filter(error => error.field !== field))
+    }
+  }, [])
+
+  //------
+  // Submission
+
+  const timer = useTimer()
+
+  const submit = React.useCallback(async (...args: any[]): Promise<SubmitResult | undefined> => {
+    const event   = isFormEvent(args[0]) ? args.shift() as React.FormEvent : null
+    const options = args.shift() ?? {} as SubmitOptions
+
+    event?.preventDefault()
+    event?.stopPropagation()
+
+    if (options.ifModified && !modified) {
+      return Promise.resolve(undefined)
+    }
+
+    if (beforeSubmit?.(model) === false) {
+      return Promise.resolve(undefined)
+    }
+
+    setSubmitting(true)
+    clearErrors()
+
+    try {
+      let result = await model.submit()
+      if (result == null) { return }
+
+      // Let the model allow some error path translations.
+      result = translateFormModelErrorPaths(result, model)
+
+      if (!timer.isDisposed) {
+        if (isSuccessResult(result)) {
+          setModified(false)
+        } else if (result.status === 'invalid') {
+          setErrorsState(errorsRef.current = result.errors)
+        }
+      }
+      afterSubmit?.(result, model)
+
+      if (isSuccessResult(result) && resetOnSuccess) {
+        model.reset?.()
+      }
+
+      return result
+    } finally {
+      if (!timer.isDisposed) {
+        setSubmitting(false)
+      }
+    }
+  }, [modified, beforeSubmit, model, clearErrors, timer.isDisposed, afterSubmit, resetOnSuccess, setModified])
+
+  //------
+  // Data & errors ref
+
+  const commit = React.useCallback(() => {
+    if (autoSubmit && modifiedRef.current) {
+      submit()
+    }
+  }, [modifiedRef, autoSubmit, submit])
+
+  const {getFieldValue, setData, onChangeFor} = useFormDataSource(
+    model,
+    {
+      modified,
+      setModified,
+      commit,
+    },
+  )
+
+  const reset = React.useCallback(() => {
+    model.reset?.()
+
+    if (initialDataRef.current != null) {
+      setData(initialDataRef.current)
+    }
+
+    clearErrors()
+    setModified(false)
+  }, [clearErrors, initialDataRef, model, setData, setModified])
+
+  React.useEffect(() => {
+    reset()
+  }, [reset])
+
+  const context = React.useMemo(() => ({
+    model,
+    dataSource: model,
+    setData,
+    getFieldValue,
+    onChangeFor,
+
+    invalid,
+    errors,
+    isInvalid,
+    errorsFor,
+    addError,
+    clearErrors,
+
+    modified,
+    setModified,
+
+    submit,
+    submitting,
+    commit,
+    reset,
+  }), [])
+
+  React.useEffect(() => {
+    if (ref == null) { return }
+    assignRef(ref, context)
+    return () => { releaseRef(ref, context) }
+  }, [])
+
+  function render() {
+    return (
+      <FormContext.Provider value={context}>
+        {translation != null ? (
+          <FormTranslationProvider translation={translation}>
+            {renderChildren()}
+          </FormTranslationProvider>
+        ) : (
+          renderChildren()
+        )}
+      </FormContext.Provider>
+    )
+  }
+
+  function renderChildren() {
+    return isFunction(children) ? children(context) : children
+  }
+
+  return render()
+
+
+})
 
 //------
 // Hook
@@ -88,3 +292,14 @@ export interface FormContainerProps {
 export function useForm<M extends FormModel>() {
   return React.useContext<FormContext<M>>(FormContext)
 }
+
+//------
+// Helpers
+
+function isFormEvent(arg: any): arg is React.FormEvent {
+  if (!isObject(arg)) { return false }
+  return (arg as React.FormEvent).nativeEvent instanceof Event
+}
+
+const emptyFieldChangeCallback = (() => void 0) as any as FieldChangeCallback<any>
+emptyFieldChangeCallback.partial = () => void 0
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,6 @@
 // @index: export * from ${relpath}
 export * from './useAutoFilledDetection'
 export * from './useFieldChangeCallback'
-export * from './useFormContainer'
 export * from './useFormDataSource'
 export * from './useFormField'
 // /index
